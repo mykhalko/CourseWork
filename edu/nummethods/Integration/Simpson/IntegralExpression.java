e8224a46e7195cf7f471b09bfbe5ca81165ccb20
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-03-24 14:14:02 +0200

    No need in IntegralExpression now, erased

--- edu/nummethods/Integration/Simpson/IntegralExpression.java
+++
@@ -1,7 +0,0 @@
-package edu.nummethods.Integration.Simpson;
-
-public interface IntegralExpression {
-
-    double calculate(double x);
-
-}
--- edu/nummethods/Rod/RodTemperatureAlteration.java
+++ edu/nummethods/Rod/RodTemperatureAlteration.java
@@ -1,10 +1,5 @@
 package edu.nummethods.Rod;
 
-import edu.nummethods.Integration.Simpson.Integral;
-import edu.nummethods.Integration.Simpson.IntegralExpression;
-
-import java.util.ArrayList;
-
 public class RodTemperatureAlteration {
 
 
