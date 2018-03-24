03460201c64c7e08c0ef0463353a1bd8f8c08c27
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-03-24 14:35:02 +0200

    Newton solution of nonlinear equations implemented

--- edu/nummethods/Equation/Nonlinear/Newton/Newton.java
+++ edu/nummethods/Equation/Nonlinear/Newton/Newton.java
@@ -1,7 +1,46 @@
 package edu.nummethods.Equation.Nonlinear.Newton;
 
+import edu.nummethods.Differentiation.Derivative;
+import edu.nummethods.Function.Multi;
+
 public class Newton {
 
+    private Multi<Double> function;
+    private Derivative derivative;
+    double equationAccuracy, derivativeAccretion;
 
+    public Newton(Multi<Double> function, double equationAccuracy, double derivativeAccretion){
+        this.function = function;
+        this.equationAccuracy = equationAccuracy;
+        this.derivativeAccretion = derivativeAccretion;
+        derivative = new Derivative(this.function, this.derivativeAccretion);
+    }
+
+    public void setEquationAccuracy(double value){
+        if(value <= 0){
+            throw new ArithmeticException("Accuracy <= 0");
+        }
+        this.equationAccuracy = value;
+    }
+
+    public void setDerivativeAccretion(double value){
+        if(value <= 0){
+            throw new ArithmeticException("Accuracy <= 0");
+        }
+        derivativeAccretion = value;
+        derivative.setAccretion(derivativeAccretion);
+    }
+
+    public double root(double startValue){
+        double result, previousResult;
+        result = startValue;
+
+        do{
+            previousResult = result;
+            result -= function.calculate(previousResult) / derivative.calculate(previousResult);
+        }while(Math.abs(result - previousResult) > equationAccuracy);
+
+        return result;
+    }
 
 }
--- edu/nummethods/main.java
+++ edu/nummethods/main.java
@@ -1,6 +1,7 @@
 package edu.nummethods;
 
 import edu.nummethods.Differentiation.Derivative;
+import edu.nummethods.Equation.Nonlinear.Newton.Newton;
 import edu.nummethods.Function.Multi;
 import edu.nummethods.Integration.Simpson.Integral;
 
@@ -13,17 +14,11 @@ public class main {
             @Override
             public Double calculate(Double... variable) {
                 double value = variable[0].doubleValue();
-                return value * value * 2;
+                return 2 * value * value - 6;
             }
         };
 
-        Derivative derivative = new Derivative(function, 0.00000001);
-
-
-        Integral integral = new Integral(function, 0.00000001);
-
-        System.out.printf("function(x0 = 4): %f derivative(x0 = 4): %f integral[0 ; 4]: %f",
-                function.calculate(4.0), derivative.calculate(4), integral.calculate(0, 4));
+        
 
         return;
     }
